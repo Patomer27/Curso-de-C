260eefa0ceda6e75fce57737262c2326a63aae58
Luidi <luidisouza2611@gmail.com>
2025-05-19 23:19:11 -0300

    OP aritimeticos

---
+++ CODIGOS/operadores_aritimeticos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+float nota1, nota2;
+printf("digite a primeira nota: "); 
+scanf("%f", &nota1);
+printf("digite a segunda nota: "); 
+scanf("%f", &nota2);
+printf("media = %f\n", (nota1+nota2)/2); 
+
+}
