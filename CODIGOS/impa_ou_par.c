d2ade3a91b8226723222fa311d20a7522bbc6776
Luidi <luidisouza2611@gmail.com>
2025-05-23 23:07:37 -0300

    atv: verificação de numero
    progama simples usando if que verifica se é impar ou par

---
+++ CODIGOS/impa_ou_par.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int n1;
+
+scanf("%d", &n1);
+
+if (n1 % 2 == 0)
+{
+    printf("o numero %d e par", n1); 
+}
+if (n1 % 2 != 0)
+{
+    printf("o numero %d e impar", n1); 
+}
+
+
+
+}
