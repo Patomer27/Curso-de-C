b6854b9ca1b5c5c29c24675deffa2fbf7bf5c59d
Luidi <luidisouza2611@gmail.com>
2025-05-23 23:26:51 -0300

    atv: positivo negativo nulo
    verifica se o numero é positivo ou negativo caso o numero digitado seja zero o valor sera nulo

---
+++ CODIGOS/valor_numero.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+int n1;
+
+scanf("%d",&n1);
+
+if (n1 > 0)
+{
+    printf("o numero %d e positivo", n1); 
+}
+if (n1 < 0)
+{
+    printf("o numero %d e negativo", n1); 
+}
+if (n1 == 0)
+{
+    printf("o numero %d e nulo", n1); 
+}
+
+
+
+}
