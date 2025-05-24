3ec164b86708e123d4b2e159bcac166b1e8bcc33
Luidi <luidisouza2611@gmail.com>
2025-05-23 23:39:02 -0300

    atv: fliper
    programa que, dadas as posições das portinhas P e R neste flíper da figura, diga por qual dos três caminhos, A, B ou C, a bolinha vai cair!

---
+++ CODIGOS/fliper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+int p, r;
+
+scanf("%d", &p);
+scanf("%d", &r);
+
+if (p == 0)
+{
+    printf("C"); 
+}
+if (p == 1 && r == 0)
+{
+    printf("B"); 
+}
+if (p == 1 && r == 1)
+{
+    printf("A"); 
+}
+
+
+
+
+}
