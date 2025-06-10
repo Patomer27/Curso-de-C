3dc4da97ca3ab6f56eeee3c382eac60e6dae3938
Luidi <luidisouza2611@gmail.com>
2025-06-10 00:01:00 -0300

    for e do while

---
+++ CODIGOS/for.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+    // Declara as variáveis inteiras n e i
+    int n;
+    int i;
+
+    // Lê um valor inteiro do usuário e armazena em n
+    scanf("%d", &n);
+
+    // Laço for: inicia i em 1, executa enquanto i <= n, incrementa i em 1 a cada iteração
+    for(i = 1; i <= n; i = i + 1){
+        // Imprime "Neps" seguido de uma quebra de linha
+        printf("Neps\n");
+    }
+   
+    /*
+    \\com o comando while, o programa executa enquanto a condicao for verdadeira
+    int n, i;
+    scanf("%d", &n);
+    
+    i = 1; // inicializacao
+    while(i<=n){ //condicao
+        printf("Neps\n");
+        i++; // incremento
+                }
+*/
+
+}
