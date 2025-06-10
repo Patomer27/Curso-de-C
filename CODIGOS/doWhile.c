2c029a128961c4e0809e2fd437de087cd007197a
Luidi <luidisouza2611@gmail.com>
2025-06-10 13:50:15 -0300

    do while

--- CODIGOS/doWhile.c
+++ CODIGOS/doWhile.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(){
-
+	int n;
+	
+	printf("digite 2024: ");
+    scanf("%d",&n);
     
-
+    if (n == 2024)
+    {
+        printf("parabens vc digitou %d",n); 
+    }
+    
+   else
+   {
+    do{
+        printf("digite 2024: "); 
+        scanf("%d",&n);
+    } while(n != 2024);{
+      printf("parabens vc digitou %d",n); }    
+}
+   
+        
+    
 }
